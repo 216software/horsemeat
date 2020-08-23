1ea59ecc58619c2785e18aecac9446de4d0b2e19
W. Matthew Wilson <mwilson@level20.com>
2020-08-23 18:21:19 -0400

    Tweaked the setup.py script since pip apparently changed its internals

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 try:
     from pip.req import parse_requirements
-except ImportError:
+except (ImportError, ModuleNotFoundError):
     # The req module has been moved to pip._internal in the 10
     # release.
     from pip._internal.req import parse_requirements
@@ -16,7 +16,7 @@ except ImportError:
 with open(os.path.join(os.getcwd(), "horsemeat", "version.py")) as f:
     exec(f.read())
 
-requirements = [str(req.req)
+requirements = [str(req.requirement) if hasattr(req, "requirement") else str(req.req)
     for req in parse_requirements(
         "requirements.txt",
         session="setup.py")]
